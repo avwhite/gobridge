139dc6f582749505fd00031f718c4b2dd0d13533
Andreas Vinter-Hviid <andreas.vinterhviid@gmail.com>
2014-06-22 21:30:25 +0200

    Change ogs.py to connect to now non-beta OGS

--- ogs.py
+++ ogs.py
@@ -7,7 +7,7 @@ with open('password.json') as f:
 
 params['grant_type'] = 'password'
 
-r = requests.post('http://beta.online-go.com/oauth2/access_token', data=params)
+r = requests.post('http://online-go.com/oauth2/access_token', data=params)
 print(r)
 print(r.text)
 
@@ -20,6 +20,6 @@ headers = {
 
 data = json.dumps({'moves': 'bc'})
 
-r = requests.get('http://beta.online-go.com/api/v1/reviews/59/', headers=headers, data=data)
+r = requests.get('http://online-go.com/api/v1/demos', headers=headers, data=data)
 
 print(r.text)
